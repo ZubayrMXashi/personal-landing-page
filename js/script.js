d48727a5358e5506f159421f26c627c504f3cd60
ZubayrMXashi <ZubayrX101@gmail.com>
2026-01-18 21:38:01 +0200

    Refactored the files to work more efficiently and clean

--- js/script.js
+++ js/script.js
@@ -38,58 +38,14 @@ for (const element of skillsSection.children) {
   }
 }
 
-const wrapper = document.createElement("div"); // Makes Wrapper for skills section
-wrapper.className = "wrapper"; // Name the Class for the skills section Wrapper to style it
-
-const TBDiv = document.createElement("div"); // Makes Wrapper for the title and the button
-TBDiv.className = "TBDiv"; // Name the Class for the title and button Wrapper to style it
-
-const ContentDiv = document.createElement("div"); // Makes Wrapper for the content section (more like holds the ul)
-ContentDiv.className = "ContentDiv"; // Name the class for the content section Wrapper to style it
-
-// Wrap the elements in the divs
-for (const element of skillsSection.children) {
-  // Get all elements in the skills section (h2 and ul)
-  if (element.tagName === "H2") {
-    element.replaceWith(TBDiv); // Replace the h2 with the div
-    TBDiv.append(element); // Put the h2 inside the div
-  } else {
-    element.replaceWith(ContentDiv); // Replace the content with the div
-    ContentDiv.append(element); // Put the content inside the div
-  }
-}
-
-const toggleBtn = document.createElement("button"); // Create the button
-const arrow = document.createElement("span"); // Create span for arrow
-arrow.textContent = "▾"; // Add the arrow into the span
-arrow.className = "arrow"; // Name the Class for the Arrow to style it
-
-toggleBtn.append(arrow); // Put the arrow inside the button
-toggleBtn.className = "toggleBtn"; // Name the Class for the button to style it
+const toggleBtn = document.getElementsByClassName("toggleBtn")[0];
+const content = document.getElementsByClassName("content")[0];
+// console.dir(content); // Just keeping this here for keeps sake
 
 // Deal with the Logic behind the button click
 toggleBtn.onclick = () => {
   console.log("button clicked"); // Console log the button being clicked
   toggleBtn.classList.toggle("btnToggled"); // Toggle the class name for the button when it is clicked
 
-  if (toggleBtn.className === "toggleBtn btnToggled") {
-    wrapper.append(ContentDiv); // If it is opened add the content (show it)
-  }
-
-  if (toggleBtn.className === "toggleBtn") {
-    ContentDiv.remove(); // If it is closed remove it (hide it)
-  }
+  content.classList.toggle("contentShown"); // Toggle the class name for the content to show it
 };
-
-TBDiv.append(toggleBtn); // add the button to the title and button div
-
-for (const element of skillsSection.children) {
-  element.replaceWith(wrapper); // Replace the original skills section with this new div-wrapped skills section
-  wrapper.append(element); // add the old skill section into this new div-wrapped skill section
-}
-
-// Make content closed (hidden/removed) in the original state, for it to be toggled (shown/hidden) with the button afterwards
-if (toggleBtn.className === "toggleBtn") {
-  const contentDiv = wrapper.children[1]; // Get a reference to the content div
-  contentDiv.remove(); // Remove the content div (hide it)
-}
